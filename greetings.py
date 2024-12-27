2aa59c6bc6deae6cdc463b01e65dfbfcbafb9a46
NickolaBK <nickolka020206@gmail.com>
2024-12-27 20:34:19 +0300

    Add files via upload

--- greetings.py
+++ greetings.py
@@ -1,7 +1,8 @@
 ﻿username = "Николай" #Имя пользователя
 title = "Заметка 1" #Название заметки
 content = "текст..." #содержание заметки
-status = "срочно" #статус заметки
+status = "активно" #статус заметки
 created_date = "23-12-2024" #дата создания
 issue_date = "25-12-2024" #дата выполнения
 print(username, title, content, status, created_date, issue_date)
+print(title)
